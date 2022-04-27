5babc79b11aca432739e4aa0af40884019f49ed3
pallobbiswas <pallobbiswas326@gmail.com>
2022-04-27 14:10:40 +0600

    'ses'

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middle ware
-app.use(cors());
-app.use(express.json());
+// Middlewares
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 
 //database info
 // name: remadeEmajon
