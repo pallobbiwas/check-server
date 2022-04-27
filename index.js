62ce3f69e0c1145bc6544aed225dced43a63e6a9
pallobbiswas <pallobbiswas326@gmail.com>
2022-04-27 15:19:42 +0600

    adddded

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const port = process.env.PORT || 5000;
 
 // Middlewares
 
-/* app.use(cors());
-app.use(express.json()); */
+app.use(cors());
+app.use(express.json());
 
-const corsConfig = {
+/* const corsConfig = {
   origin: true,
   credentials: true,
 };
 app.use(cors(corsConfig));
-app.options("*", cors(corsConfig));
+app.options("*", cors(corsConfig)); */
 
 //database info
 // name: remadeEmajon
